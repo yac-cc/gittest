16c10bcf612144565ad963fdd87f250100e876c3
yac_cc <cclion62@gmail.com>
2021-05-05 11:57:22 +0800

    TCFSH s003 AC #003

--- python3_note/APCS_Topic/TCFSH_a003.py
+++ python3_note/APCS_Topic/TCFSH_a003.py
@@ -3,10 +3,12 @@
 def kk(x):
     return ord(x)
 try:
-    nbs =list(map(kk,list(input())))
-    ew=()
-    for i in range(6):
-        ew.attend(abs(nbs(i+1)-nbs(i)))
-    "".join(ew)
+    while True :
+        nbs =list(map(kk,list(input())))
+        ew=''
+        for i in range(6):
+            k=abs(nbs[i+1]-nbs[i])
+            ew=ew + str(k)
+        print(ew)
 except EOFError :
     pass 
